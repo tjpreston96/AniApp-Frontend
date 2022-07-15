3ebab7757d5c602eb608ef988b1fdbadc3384d90
Tyler Preston <tpreston96@gmail.com>
2022-07-15 16:42:12 -0500

    update Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
-import styles from './Landing.module.css'
+import styles from "./Landing.module.css";
+import Clock from "../../components/Clock/Clock";
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Hello, {user ? user.name : "friend"}</h1>
+      <Clock />
     </main>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
