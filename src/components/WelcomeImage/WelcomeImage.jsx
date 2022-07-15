6126eac43317c86d75486888a3565ab14cf56ee1
Tyler Preston <tpreston96@gmail.com>
2022-07-15 18:34:22 -0500

    Create WelcomeImage

---
+++ src/components/WelcomeImage/WelcomeImage.jsx
@@ -0,0 +1,7 @@
+import Pikachu from "../../assets/giphy-9.gif";
+
+const WelcomeImage = () => {
+  return <img src={Pikachu} className="welcomeImg" alt="pikachu" />;
+};
+
+export default WelcomeImage;
