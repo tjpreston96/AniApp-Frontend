251aa033f662e1928fa814ca15df1cacb405a82b
Tyler Preston <tpreston96@gmail.com>
2022-07-15 22:35:41 -0500

    set up state change for conditional search

--- src/components/NavSearch/NavSearch.jsx
+++ src/components/NavSearch/NavSearch.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 
 const NavSearch = () => {
-  const [type, setType] = useState(null);
-  
+  const [type, setType] = useState("anime");
+
   return (
     <form className="d-flex">
       <div className="input-group">
-        <span className="input-group-text" id="basic-addon1">
-          <i className="bi bi-book" />
-        </span>
-        <span className="input-group-text btn btn-dark" id="basic-addon1">
+        <span
+          className={
+            type === "anime"
+              ? "input-group-text btn btn-secondary"
+              : "input-group-text"
+          }
+          id="basic-addon2 "
+          onClick={() => setType("anime")}
+        >
           <i className="bi bi-tv" />
         </span>
+
+        <span
+          className={
+            type === "manga"
+              ? "input-group-text btn btn-secondary"
+              : "input-group-text"
+          }
+          id="basic-addon1"
+          onClick={() => setType("manga")}
+        >
+          <i className="bi bi-book" />
+        </span>
+        
         <input
           type="text"
           className="form-control"
@@ -19,6 +37,10 @@ const NavSearch = () => {
           aria-label="Search Titles"
           aria-describedby="basic-addon1"
         />
+
+        <button className="btn btn-secondary" type="submit">
+          Search
+        </button>
       </div>
     </form>
   );
