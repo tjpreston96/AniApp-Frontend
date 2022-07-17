843c1c6ec1841668f954cb6d275843a360841631
Tyler Preston <tpreston96@gmail.com>
2022-07-16 23:55:34 -0500

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = ({ user, handleLogout }) => {
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li >Welcome, {user.name}</li>
+                {/* <li className="nav-item ">Welcome, {user.name}</li> */}
 
                 <li className="nav-item dropdown">
                   <Link
@@ -122,15 +122,54 @@ const NavBar = ({ user, handleLogout }) => {
           </div>
         </nav>
       ) : (
-        <nav className="navbar navbar-expand-lg">
-          <ul>
-            <li>
-              <Link to="/login">Log In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand">
+              AniApp
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle"
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Accounts
+                  </Link>
+                  <ul
+                    className="dropdown-menu "
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li className="dropdown-item">
+                      <Link to="/login">Log In</Link>
+                    </li>
+                    <li className="dropdown-item">
+                      <Link to="/signup">Sign Up</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       )}
     </>
