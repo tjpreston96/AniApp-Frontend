ca7a4becd6c6daf3d92d02dd98ca6952844a878f
Tyler Preston <tpreston96@gmail.com>
2022-07-16 23:47:28 -0500

    format Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -12,14 +12,13 @@ const Landing = ({ user }) => {
       <h1>Hello, {user ? user.name : "Friend"}</h1>
       <Clock />
       <WelcomeImage />
-      
-        <h3>Popular Anime</h3>
-        <Carousel type="anime" collection="1,13593,45398" />
-        <hr />
 
-        <h3>Popular Manga</h3>
-        <Carousel type="manga" collection="38,1531,40815" />
-      
+      <h3>Popular Anime</h3>
+      <Carousel type="anime" collection="1,13593,45398" />
+      <hr />
+
+      <h3>Popular Manga</h3>
+      <Carousel type="manga" collection="38,1531,40815" />
     </main>
   );
 };
