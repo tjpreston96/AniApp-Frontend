f1ea6ca5a4bef50b0320b1920fe3271f4a9b6ed4
Tyler Preston <tpreston96@gmail.com>
2022-07-16 23:46:37 -0500

    Carousel to Landing page (no positioning)

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import styles from "./Landing.module.css";
 import Clock from "../../components/Clock/Clock";
 import WelcomeImage from "../../components/WelcomeImage/WelcomeImage";
+import Carousel from "../../components/Carousel/Carousel";
 
 const Landing = ({ user }) => {
+  useEffect(() => {}, []);
+
   return (
     <main className={styles.container}>
-      <h1>Hello, {user ? user.name : "friend"}</h1>
+      <h1>Hello, {user ? user.name : "Friend"}</h1>
       <Clock />
       <WelcomeImage />
+      
+        <h3>Popular Anime</h3>
+        <Carousel type="anime" collection="1,13593,45398" />
+        <hr />
+
+        <h3>Popular Manga</h3>
+        <Carousel type="manga" collection="38,1531,40815" />
+      
     </main>
   );
 };
