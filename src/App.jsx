8cf0473efaceb4dfc86ee8bfb96c18db1e5e200e
Tyler Preston <tpreston96@gmail.com>
2022-07-17 23:49:21 -0500

    add search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
-import Signup from './pages/Signup/Signup'
-import Login from './pages/Login/Login'
-import Landing from './pages/Landing/Landing'
-import Profiles from './pages/Profiles/Profiles'
-import ChangePassword from './pages/ChangePassword/ChangePassword'
-import * as authService from './services/authService'
+import { useState } from "react";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Search from "./pages/Search/Search";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Landing from "./pages/Landing/Landing";
+import Profiles from "./pages/Profiles/Profiles";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import * as authService from "./services/authService";
 
 const App = () => {
-  const [user, setUser] = useState(authService.getUser())
-  const navigate = useNavigate()
+  const [user, setUser] = useState(authService.getUser());
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    authService.logout()
-    setUser(null)
-    navigate('/')
-  }
+    authService.logout();
+    setUser(null);
+    navigate("/");
+  };
 
   const handleSignupOrLogin = () => {
-    setUser(authService.getUser())
-  }
+    setUser(authService.getUser());
+  };
 
   return (
     <>
       <NavBar user={user} handleLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<Landing user={user} />} />
+        <Route path="/search" element={<Search user={user} />} />
+
         <Route
           path="/signup"
           element={<Signup handleSignupOrLogin={handleSignupOrLogin} />}
@@ -51,7 +54,7 @@ const App = () => {
         />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
