5d201f4704633f19354bdb280c0ab48ec9c60bd8
Tyler Preston <tpreston96@gmail.com>
2022-07-17 23:50:52 -0500

    refactor service functions

--- src/services/mediaService.js
+++ src/services/mediaService.js
@@ -3,14 +3,29 @@ import * as tokenService from "./tokenService";
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/media`;
 
 async function getCollection(type, collection) {
-  const res = await fetch(`${BASE_URL}/collection/${type}/${collection}`, {
-    method: "GET",
+  const res = await fetch(`${BASE_URL}/collection/${type}`, {
+    method: "POST",
     headers: {
       "Authorization": `Bearer ${tokenService.getToken()}`,
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({ collection }),
   });
+
   return res.json();
 }
 
-export { getCollection };
+async function search(type, title) {
+  const res = await fetch(`${BASE_URL}/search/${type}`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${tokenService.getToken()}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title }),
+  });
+
+  return res.json();
+}
+
+export { getCollection, search };
