9a6a3861f9986525abc3d4e1791d3218ea70a5bd
Tyler Preston <tpreston96@gmail.com>
2022-07-17 23:55:09 -0500

    WIP - Style Landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import styles from "./Landing.module.css";
+// import styles from "./Landing.module.css";
 import Clock from "../../components/Clock/Clock";
 import WelcomeImage from "../../components/WelcomeImage/WelcomeImage";
 import Carousel from "../../components/Carousel/Carousel";
@@ -8,17 +8,25 @@ const Landing = ({ user }) => {
   useEffect(() => {}, []);
 
   return (
-    <main className={styles.container}>
-      <h1>Hello, {user ? user.name : "Friend"}</h1>
+    <main className="container container-fluid">
+      <h1 className="text-center">Hello, {user ? user.name : "Friend"}</h1>
       <Clock />
-      <WelcomeImage />
 
-      <h3>Popular Anime</h3>
-      <Carousel type="anime" collection="1,13593,45398" />
-      <hr />
-
-      <h3>Popular Manga</h3>
-      <Carousel type="manga" collection="38,1531,40815" />
+      <div className="row justify-content-center">
+        <div className="col-sm-6 col-md-6">
+          <WelcomeImage />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-5">
+          <h3 className="text-center">Popular Anime</h3>
+          <Carousel type="anime" collection="1,13593,45398" />
+        </div>
+        <div className="col-5">
+          <h3 className="text-center">Popular Manga</h3>
+          <Carousel type="manga" collection="38,57,40815" />
+        </div>
+      </div>
     </main>
   );
 };
