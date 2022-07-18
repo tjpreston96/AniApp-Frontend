962f362aa092f7c7266c756eb6301cf43fc8f123
Tyler Preston <tpreston96@gmail.com>
2022-07-18 01:13:04 -0500

    Stub Card Top

--- src/components/ResultDetail/ResultDetail.jsx
+++ src/components/ResultDetail/ResultDetail.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const ResultDetail = ({ result }) => {
-  return <div></div>;
+  return (
+    <div className="card">
+      {result.type === "anime" ? (
+        <iframe
+          title="trailer"
+          className="ytPlayer"
+          src={`https://www.youtube.com/embed/${result.attributes.youtubeVideoId}`}
+          frameBorder="0"
+          allowFullScreen
+        />
+      ) : (
+        <img
+          src={result.attributes.posterImage.large}
+          className="card-img-top"
+          alt="card"
+        />
+      )}
+    </div>
+  );
 };
 
 export default ResultDetail;
