e8a52ab82eed6e234c348702c37b07655d70f23c
Tyler Preston <tpreston96@gmail.com>
2022-07-17 23:53:53 -0500

    rename NavSearch to SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,41 +1,52 @@
 import { useState } from "react";
+import { search } from "../../services/mediaService";
 
-const NavSearch = () => {
+const SearchBar = ({ setResults }) => {
   const [type, setType] = useState("anime");
+  const [title, setTitle] = useState("");
+
+  const onInputChange = (e) => setTitle(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    search(type, title).then((response) => setResults(response));
+  };
 
   return (
-    <form className="d-flex">
+    <form onSubmit={onSubmit} className="d-flex">
       <div className="input-group">
         <span
           className={
             type === "anime"
-              ? "input-group-text btn btn-secondary"
-              : "input-group-text"
+              ? "input-group-text btn btn-dark"
+              : "input-group-text btn btn-secondary"
           }
           id="basic-addon2 "
           onClick={() => setType("anime")}
         >
-          <i className="bi bi-tv" />
+          <i className="bi bi-tv" /> Anime
         </span>
 
         <span
           className={
             type === "manga"
-              ? "input-group-text btn btn-secondary"
-              : "input-group-text"
+              ? "input-group-text btn btn-dark"
+              : "input-group-text btn btn-secondary"
           }
           id="basic-addon1"
           onClick={() => setType("manga")}
         >
-          <i className="bi bi-book" />
+          <i className="bi bi-book" /> Manga
         </span>
-        
+
         <input
           type="text"
           className="form-control"
           placeholder="Search Titles..."
           aria-label="Search Titles"
           aria-describedby="basic-addon1"
+          value={title}
+          onChange={onInputChange}
         />
 
         <button className="btn btn-secondary" type="submit">
@@ -46,4 +57,4 @@ const NavSearch = () => {
   );
 };
 
-export default NavSearch;
+export default SearchBar;
