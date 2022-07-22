ca12ffe2e6455aa5ff91eeaaeb65ba3a3f001ac2
Tyler Preston <tpreston96@gmail.com>
2022-07-22 14:07:18 -0500

    anime and manga collection

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ResultsList from "../../components/ResultsList/ResultsList";
 import ResultDetail from "../../components/ResultDetail/ResultDetail";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import { userCollection } from "../../services/mediaService";
 
 const Search = () => {
   const [results, setResults] = useState(null);
   const [selectedResult, setSelectedResult] = useState(null);
   const [selectedIdx, setSelectedIdx] = useState(null);
-  // const [collection, setCollection] = useState([]);
+  const [mangaCollection, setMangaCollection] = useState(null);
+  const [animeCollection, setAnimeCollection] = useState(null);
 
-  // const handleSetCollection = () => {
-
-  // };
+  useEffect(() => {
+    async function setCollections() {
+      setMangaCollection(await userCollection("manga"));
+      setAnimeCollection(await userCollection("anime"));
+    }
+    setCollections();
+  }, []);
 
   return (
     <div className="container">
@@ -30,14 +36,12 @@ const Search = () => {
           <hr className="d-lg-none" />
           {/* List Group */}
           <div className="col-12 col-lg-4">
-            <div className="list-group">
-              <ResultsList
-                results={results}
-                setSelectedResult={setSelectedResult}
-                setSelectedIdx={setSelectedIdx}
-                selectedIdx={selectedIdx}
-              />
-            </div>
+            <ResultsList
+              results={results}
+              setSelectedResult={setSelectedResult}
+              setSelectedIdx={setSelectedIdx}
+              selectedIdx={selectedIdx}
+            />
           </div>
         </div>
       )}
