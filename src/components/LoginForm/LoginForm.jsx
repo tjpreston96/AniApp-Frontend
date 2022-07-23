8369a00a3f5342fa7c6de99e63f7eac51a1b325d
Tyler Preston <tpreston96@gmail.com>
2022-07-22 21:14:57 -0500

    Login Form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,67 +1,71 @@
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import styles from './LoginForm.module.css'
-import * as authService from '../../services/authService'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./LoginForm.module.css";
+import * as authService from "../../services/authService";
 
-const LoginForm = props => {
+const LoginForm = (props) => {
   const [formData, setFormData] = useState({
-    email: '',
-    pw: '',
-  })
-  const navigate = useNavigate()
+    email: "",
+    pw: "",
+  });
+  const navigate = useNavigate();
 
-  const handleChange = e => {
-    props.updateMessage('')
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+  const handleChange = (e) => {
+    props.updateMessage("");
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-  const handleSubmit = async evt => {
-    evt.preventDefault()
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
     try {
-      await authService.login(formData)
-      props.handleSignupOrLogin()
-      navigate('/')
+      await authService.login(formData);
+      props.handleSignupOrLogin();
+      navigate("/");
     } catch (err) {
-      props.updateMessage(err.message)
+      props.updateMessage(err.message);
     }
-  }
+  };
 
   return (
-    <form
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      className={styles.container}
-    >
-      <div className={styles.inputContainer}>
-        <label htmlFor="email" className={styles.label}>Email</label>
-        <input
-          type="text"
-          autoComplete="off"
-          id="email"
-          value={formData.email}
-          name="email"
-          onChange={handleChange}
-        />
+    <form autoComplete="off" onSubmit={handleSubmit} className=" col-8 col-sm-6 col-lg-5 col-xxl-4">
+      <div className="">
+        <div className="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">
+            Email
+          </span>
+          <input
+            type="text"
+            autoComplete="off"
+            className="form-control "
+            id="email"
+            value={formData.email}
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">
+            Password
+          </span>
+          <input
+            type="password"
+            autoComplete="off"
+            className="form-control "
+            id="password"
+            value={formData.pw}
+            name="pw"
+            onChange={handleChange}
+          />
+        </div>
       </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="password" className={styles.label}>Password</label>
-        <input
-          type="password"
-          autoComplete="off"
-          id="password"
-          value={formData.pw}
-          name="pw"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <button className={styles.button}>Log In</button>
-        <Link to="/">
-          <button>Cancel</button>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-info me-3">Log In</button>
+        <Link to="/" className="btn btn-secondary">
+          Cancel
         </Link>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
