2b62f7f3d4f88bb212cad1b00086294bd30410e4
Tyler Preston <tpreston96@gmail.com>
2022-07-26 17:24:28 -0500

    Collection structure

---
+++ src/pages/Collection/Collection.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+// import { userCollection } from "../../services/mediaService";
+
+const Collection = () => {
+  let location = useLocation();
+  const [type, setType] = useState(location.state.type);
+  const [results, setResults] = useState(null);
+  const [selectedResult, setSelectedResult] = useState(null);
+  const [selectedIdx, setSelectedIdx] = useState(null);
+
+  const setLocation = () => setType(location.state.type);
+
+  useEffect(() => {
+    setLocation();
+  });
+
+  return (
+    <h1 className="text-white text-center">
+      {type.charAt(0).toUpperCase() + type.slice(1)} Collection
+    </h1>
+  );
+};
+
+export default Collection;
