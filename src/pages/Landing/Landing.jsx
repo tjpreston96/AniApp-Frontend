c8f27a0a654710e50765d950fd214b8fa9b097f4
Tyler Preston <tpreston96@gmail.com>
2022-07-27 22:45:09 -0500

    update Landing carousels

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,21 +9,23 @@ const Landing = ({ user }) => {
 
   return (
     <main className="container container-fluid">
-      <h1 className="text-center">Hello, {user ? user.name : "Friend"}</h1>
+      <h1 className="text-center text-white">
+        Hello, {user ? user.name : "Friend"}
+      </h1>
       <Clock />
 
       <div className="row justify-content-center">
-        <div className="col-sm-6 col-md-6">
+        <div className="col-sm-6">
           <WelcomeImage />
         </div>
       </div>
-      <div className="row justify-content-center">
-        <div className="col-5">
-          <h3 className="text-center">Popular Anime</h3>
+      <div className="row justify-content-center d-flex">
+        <div className="col col-10 col-md-5">
+          <h3 className="text-center text-white">Popular Anime</h3>
           <Carousel type="anime" collection="1,13593,45398" />
         </div>
-        <div className="col-5">
-          <h3 className="text-center">Popular Manga</h3>
+        <div className="col col-10 col-md-5">
+          <h3 className="text-center text-white">Popular Manga</h3>
           <Carousel type="manga" collection="38,57,40815" />
         </div>
       </div>
