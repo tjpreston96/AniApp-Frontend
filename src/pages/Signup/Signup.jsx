ca168b5ac2885e1924bfb3218ef8aee8f78fd17b
Tyler Preston <tpreston96@gmail.com>
2022-07-27 22:47:00 -0500

    update UI

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import SignupForm from '../../components/SignupForm/SignupForm'
-import styles from './Signup.module.css'
+import { useState } from "react";
+import SignupForm from "../../components/SignupForm/SignupForm";
 
-const Signup = props => {
-  const [message, setMessage] = useState([''])
 
-  const updateMessage = msg => {
-    setMessage(msg)
-  }
+const Signup = (props) => {
+  const [message, setMessage] = useState([""]);
+
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
 
   return (
-    <main className={styles.container}>
-      <h1>Sign Up</h1>
+    <main className="container">
+      <h1 className="text-white text-center">AniApp</h1>
       <p>{message}</p>
       <SignupForm {...props} updateMessage={updateMessage} />
     </main>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
