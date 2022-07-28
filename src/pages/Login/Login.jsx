e381e48d317257067e16e41b0aedbf720df8cada
Tyler Preston <tpreston96@gmail.com>
2022-07-27 22:45:35 -0500

    update UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import styles from "./Login.module.css";
 
 const LoginPage = (props) => {
   const [message, setMessage] = useState([""]);
@@ -10,8 +9,8 @@ const LoginPage = (props) => {
   };
 
   return (
-    <main className={styles.container}>
-      <h1 className="text-white">Log In</h1>
+    <main className="container">
+      <h1 className="text-white text-center">AniApp</h1>
       <p>{message}</p>
       <LoginForm
         handleSignupOrLogin={props.handleSignupOrLogin}
