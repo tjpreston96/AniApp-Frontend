441568412bc5afa52d447d20aebd82bef7a3be28
Tyler Preston <tpreston96@gmail.com>
2022-07-28 20:50:23 -0500

    Update UI

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,31 +1,72 @@
-import { useState, useEffect } from 'react'
-import * as profileService from '../../services/profileService'
+import { useState, useEffect } from "react";
+import * as profileService from "../../services/profileService";
+import { DateTime } from "luxon";
 
 const Profiles = () => {
-  const [profiles, setProfiles] = useState([])
+  const [profiles, setProfiles] = useState([]);
+  const [memberCount, setMemberCount] = useState(null);
 
   useEffect(() => {
     const fetchProfiles = async () => {
-      const profileData = await profileService.getAllProfiles()
-      setProfiles(profileData)
-    }
-    fetchProfiles()
-  }, [])
+      const profileData = await profileService.getAllProfiles();
+      setProfiles(profileData);
+      setMemberCount(profileData.length);
+    };
+    fetchProfiles();
+  }, []);
+
+  const profileMap = profiles.map((profile) => {
+    const partialEmail = profile.email.replace(
+      /(\w{2})[\w.-]+@([\w.]+\w)/,
+      "$1******@$2"
+    );
+
+    const formattedDate = DateTime.fromISO(profile.createdAt).toLocaleString({
+      weekday: "long",
+      month: "long",
+      day: "2-digit",
+    });
+
+    return (
+      <div className="col" key={profile._id}>
+        <div class="card h-100" style={{ maxWidth: 540 }}>
+          <div class="row g-0">
+            <div class="col-4">
+              <img
+                src={profile.photo}
+                class="img-fluid rounded-start h-100"
+                alt="..."
+                style={{ maxHeight: 130 }}
+              />
+            </div>
+            <div class="col-8">
+              <div class="card-body">
+                <h5 class="card-title">{profile.name}</h5>
+                <p class="card-text">{partialEmail}</p>
+                <p class="card-text">
+                  <small class="text-muted">Created: {formattedDate}</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
-      {profiles.length ? 
-        <>
-          {profiles.map(profile =>
-            <p key={profile._id}>{profile.name}</p>
-          )}
-        </>
-      :
+    <div className="container">
+      <h1 className="text-center text-white">Profile List</h1>
+      <h5 className="text-center text-white">{memberCount} Profiles</h5>
+      {profiles.length ? (
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2">
+          {profileMap}
+        </div>
+      ) : (
         <p>No profiles yet</p>
-      }
-    </>
-  )
-}
- 
-export default Profiles
+      )}
+    </div>
+  );
+};
+
+export default Profiles;
