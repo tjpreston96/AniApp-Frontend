3f83b0963c7c7ba57dfbc39aab2b8df7c9f6a2b8
Tyler Preston <tpreston96@gmail.com>
2022-07-28 21:11:31 -0500

    Update UI for Collection

--- src/pages/Collection/Collection.jsx
+++ src/pages/Collection/Collection.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { userCollection } from "../../services/mediaService";
 import ResultsList from "../../components/ResultsList/ResultsList";
 import CollectionDetail from "../../components/CollectionDetail/CollectionDetail";
@@ -7,12 +7,14 @@ import CollectionDetail from "../../components/CollectionDetail/CollectionDetail
 const Collection = () => {
   let location = useLocation();
   const [type, setType] = useState(location.state.type);
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState({});
   const [resultsLength, setResultsLength] = useState(null);
   const [selectedResult, setSelectedResult] = useState(null);
   const [selectedIdx, setSelectedIdx] = useState(null);
 
   const setLocation = () => setType(location.state.type);
+
+  const capType = type.charAt(0).toUpperCase() + type.slice(1);
 
   async function setCollectionResults(initIdx = 0) {
     await userCollection(type).then((response) => {
@@ -33,10 +35,8 @@ const Collection = () => {
 
   return (
     <div className="container">
-      <h1 className="text-white text-center">
-        {type.charAt(0).toUpperCase() + type.slice(1)} Collection
-      </h1>
-      {results && (
+      <h1 className="text-white text-center">{capType} Collection</h1>
+      {results.length ? (
         <div className="row">
           <div className="col-12 col-lg-8">
             {selectedResult && (
@@ -57,6 +57,26 @@ const Collection = () => {
               setSelectedIdx={setSelectedIdx}
               selectedIdx={selectedIdx}
             />
+          </div>
+        </div>
+      ) : (
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 col-md-8">
+            <div className="card text-center">
+              <div className="card-header fw-bold">
+                {capType} Collection is currently empty.
+              </div>
+              <div className="card-body">
+                <h6 className="card-title">Add your favorite {type}.</h6>
+                <Link
+                  to="/search"
+                  className="btn btn-info col-3 col-lg-3"
+                  state={{ type: `${type}` }}
+                >
+                  Search <i className="bi bi-search d-inline" />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       )}
