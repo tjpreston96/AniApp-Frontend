b0c299b970b6f0974e6a03dc16effd95ff371980
Tyler Preston <tpreston96@gmail.com>
2022-08-04 02:52:26 -0500

    update UI

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,28 +8,26 @@ const Landing = ({ user }) => {
   useEffect(() => {}, []);
 
   return (
-    <main className="container container-fluid">
+    <>
       <h1 className="text-center text-white">
         Hello, {user ? user.name : "Friend"}
       </h1>
       <Clock />
 
       <div className="row justify-content-center">
-        <div className="col-sm-6">
-          <WelcomeImage />
-        </div>
+        <WelcomeImage />
       </div>
       <div className="row justify-content-center d-flex">
-        <div className="col col-10 col-md-5">
+        <div className="col col-8 col-md-5">
           <h3 className="text-center text-white">Popular Anime</h3>
           <Carousel type="anime" collection="1,13593,45398" />
         </div>
-        <div className="col col-10 col-md-5">
+        <div className="col col-8 col-md-5">
           <h3 className="text-center text-white">Popular Manga</h3>
           <Carousel type="manga" collection="38,57,40815" />
         </div>
       </div>
-    </main>
+    </>
   );
 };
 
