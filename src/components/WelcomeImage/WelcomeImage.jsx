9b11d2521ebd538181ddefe55c25c1b3df5ff602
Tyler Preston <tpreston96@gmail.com>
2022-08-04 02:51:58 -0500

    update image

--- src/components/WelcomeImage/WelcomeImage.jsx
+++ src/components/WelcomeImage/WelcomeImage.jsx
@@ -1,7 +1,7 @@
 import Pikachu from "../../assets/giphy-9.gif";
 
 const WelcomeImage = () => {
-  return <img src={Pikachu} className="mx-auto d-block" alt="pikachu" />;
+  return <img src={Pikachu} className="mx-auto d-block col-9 col-md-5" alt="pikachu" />;
 };
 
 export default WelcomeImage;
