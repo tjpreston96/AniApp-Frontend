589e9bfa3c255b9c987553774ba86634b4727791
Tyler Preston <tpreston96@gmail.com>
2022-08-04 03:23:05 -0500

    update NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
+    <header>
       {user ? (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
           <div className="container-fluid">
@@ -206,7 +206,7 @@ const NavBar = ({ user, handleLogout }) => {
           </div>
         </nav>
       )}
-    </>
+    </header>
   );
 };
 
